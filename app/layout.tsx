095e6c2576d9c08e6ec6d99e120d6aa098d43dbe
Pedro Duarte <oi@ped.ro>
2023-06-06 17:48:55 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { Providers } from "@/components/providers";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { getAllThemes } from "@/lib/theme";
 import { ThemeFilter } from "@/components/theme-filter";
-import { Suspense } from "react";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +24,7 @@ export default async function RootLayout({
       <body className={`${inter.className}`}>
         <Providers themes={themes}>
           <div className="flex flex-col h-screen">
-            {children}
+            <div className="flex flex-col flex-1">{children}</div>
             <div className="flex justify-between pt-5 px-5 bg-[--bg]">
               <ThemeFilter themes={themes} />
               <button className="rounded-2 border h-[30px] inline-flex px-3 items-center text-3">
